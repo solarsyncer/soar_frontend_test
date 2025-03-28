da04f03eec04c48e72fc9578fa069a4a9082e9d2
solarsyncer <29234432+solarsyncer@users.noreply.github.com>
2025-03-29 06:12:17 +0900

    add constants

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,63 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image from "next/image";
-
-const navigation = [
-  {
-    name: "Dashboard",
-    href: "/dashboard",
-    icon: "/icons/sidebar/dashboard.svg",
-    enabled: true,
-  },
-  {
-    name: "Transactions",
-    href: "#",
-    icon: "/icons/sidebar/transactions.svg",
-    enabled: false,
-  },
-  {
-    name: "Accounts",
-    href: "#",
-    icon: "/icons/sidebar/accounts.svg",
-    enabled: false,
-  },
-  {
-    name: "Investments",
-    href: "#",
-    icon: "/icons/sidebar/investments.svg",
-    enabled: false,
-  },
-  {
-    name: "Credit Cards",
-    href: "#",
-    icon: "/icons/sidebar/credit_cards.svg",
-    enabled: false,
-  },
-  {
-    name: "Loans",
-    href: "#",
-    icon: "/icons/sidebar/loans.svg",
-    enabled: false,
-  },
-  {
-    name: "Services",
-    href: "#",
-    icon: "/icons/sidebar/services.svg",
-    enabled: false,
-  },
-  {
-    name: "My Privileges",
-    href: "#",
-    icon: "/icons/sidebar/my_privileges.svg",
-    enabled: false,
-  },
-  {
-    name: "Setting",
-    href: "/settings",
-    icon: "/icons/sidebar/settings.svg",
-    enabled: true,
-  },
-];
+import { navigation } from "@/constants/sidebarItems";
 
 interface SidebarProps {
   isOpen: boolean;
---
+++ src/constants/sidebarItems.ts
@@ -0,0 +1,56 @@
+export const navigation = [
+  {
+    name: "Dashboard",
+    href: "/dashboard",
+    icon: "/icons/sidebar/dashboard.svg",
+    enabled: true,
+  },
+  {
+    name: "Transactions",
+    href: "#",
+    icon: "/icons/sidebar/transactions.svg",
+    enabled: false,
+  },
+  {
+    name: "Accounts",
+    href: "#",
+    icon: "/icons/sidebar/accounts.svg",
+    enabled: false,
+  },
+  {
+    name: "Investments",
+    href: "#",
+    icon: "/icons/sidebar/investments.svg",
+    enabled: false,
+  },
+  {
+    name: "Credit Cards",
+    href: "#",
+    icon: "/icons/sidebar/credit_cards.svg",
+    enabled: false,
+  },
+  {
+    name: "Loans",
+    href: "#",
+    icon: "/icons/sidebar/loans.svg",
+    enabled: false,
+  },
+  {
+    name: "Services",
+    href: "#",
+    icon: "/icons/sidebar/services.svg",
+    enabled: false,
+  },
+  {
+    name: "My Privileges",
+    href: "#",
+    icon: "/icons/sidebar/my_privileges.svg",
+    enabled: false,
+  },
+  {
+    name: "Setting",
+    href: "/settings",
+    icon: "/icons/sidebar/settings.svg",
+    enabled: true,
+  },
+];
